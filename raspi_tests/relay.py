3852efe3431c889611b03917c428fbf317032e64
Yakul <xayide.rg@gmail.com>
2019-12-07 20:30:45 +0100

    jeje

--- raspi_tests/relay.py
+++ raspi_tests/relay.py
@@ -2,14 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
 
 while True:
     print "Relay on"
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
     time.sleep(3)
     print "Relay off"
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
 
 
 
