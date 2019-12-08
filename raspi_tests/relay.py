bbab3fda43677995ed5345ab01cbaca806bc56c3
Yakul <xayide.rg@gmail.com>
2019-12-08 23:01:40 +0100

    relay

--- raspi_tests/relay.py
+++ raspi_tests/relay.py
@@ -7,10 +7,10 @@ GPIO.setup(24, GPIO.OUT)
 while True:
     print "Relay on"
     GPIO.output(24, GPIO.HIGH)
-    time.sleep(1.5)
+    time.sleep(10)
     print "Relay off"
     GPIO.output(24, GPIO.LOW)
-    time.sleep(1.5)
+    time.sleep(10)
 
 
 
